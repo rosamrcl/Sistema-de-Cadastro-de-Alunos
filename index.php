815eac0b142c02bb0635e4878db70e05b8fe405e
Rosa <164552124+rosamrcl@users.noreply.github.com>
2025-05-13 17:04:39 -0300

    .

--- index.php
+++ index.php
@@ -8,41 +8,46 @@ $result = $conn->query($sql);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Formulário</title>
 </head>
 <body>
     <section class="inserir" id="inserir">
+
         <form action="inserir.php" method="POST">
             <label for="nome">Nome</label>
-            <input type="text" name="nome" id="nome" placeholder="Nome">
+            <input class="box" type="text" name="nome" id="nome" placeholder="Nome">
             <label for="sobrenome">Sobrenome</label>
-            <input type="text" name="sobrenome" id="sobrenome" placeholder="Sobrenome">
+            <input class="box" type="text" name="sobrenome" id="sobrenome" placeholder="Sobrenome">
             <label for="email">E-mail</label>
-            <input type="email" name="email" id="email" placeholder="E-mail">
-            <input type="submit" value="Enviar">
+            <input class="box" type="email" name="email" id="email" placeholder="E-mail">
+            <input class="btn" type="submit" value="Enviar">
         </form>
         
     </section>
     <section class="listar">
-        <h2>Alunos Cadastrados</h2>
-        <table>
-            <tr>
-                <th>ID</th>
-                <th>Nome</th>
-                <th>Sobrenome</th>
-                <th>E-mail</th>
-            </tr>
-            <?php while ($aluno = $result->fetch_assoc()): ?>
+        <div class="box">
+            <h2>Alunos Cadastrados</h2>
+            <table>
                 <tr>
-                    <td><? $aluno['id'];?></td>
-                    <td><?=$aluno['nome'];?></td>
-                    <td><?=$aluno['sobrenome'];?></td>
-                    <td><?=$aluno['email'];?></td>
-                    <td><a class="delete-btn" href="excluir.php?id=<?=$aluno['id']; ?>" onclick="return confirm ('Tem certeza que deseja excluir?')">Excluir</a></td>             
-                    
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Sobrenome</th>
+                    <th>E-mail</th>
                 </tr>
-                <?php endwhile; ?>
-        </table>        
+                <?php while ($aluno = $result->fetch_assoc()): ?>
+                    <tr>
+                        <td><?=$aluno['id'];?></td>
+                        <td><?=$aluno['nome'];?></td>
+                        <td><?=$aluno['sobrenome'];?></td>
+                        <td><?=$aluno['email'];?></td>
+                        <td><a class="delete-btn" href="excluir.php?id=<?=$aluno['id']; ?>" onclick="return confirm ('Tem certeza que deseja excluir?')">Excluir</a></td>             
+                        
+                    </tr>
+                    <?php endwhile; ?>
+            </table>       
+
+        </div>
     
         
     </section>
