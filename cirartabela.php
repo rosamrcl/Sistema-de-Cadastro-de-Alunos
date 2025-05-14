584a421b9e4009571151abcddf83ed460518a7f8
Rosa <164552124+rosamrcl@users.noreply.github.com>
2025-05-14 14:57:17 -0300

    .

--- cirartabela.php
+++ cirartabela.php
@@ -1,7 +1,6 @@
 <?php
 include('conexao.php');
 
-$conn=new mysqli($host,$user, $pass,$db);
 
 $q="CREATE TABLE cadastro (id int AUTO_INCREMENT PRIMARY KEY , nome VARCHAR(255) NOT NULL, sobrenome VARCHAR(255)NOT NULL, email VARCHAR(255) UNIQUE NOT NULL)";
 
